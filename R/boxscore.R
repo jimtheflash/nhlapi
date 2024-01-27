d038bf5f2e30a4546427fec7ec2be993f940b972
jimtheflash <jim.kloet@gmail.com>
2024-01-27 16:59:22 -0600

    add boxscore stem

--- R/boxscore.R
+++ R/boxscore.R
@@ -10,62 +10,25 @@ get_boxscore <- function(game_id) {
 
 }
 
-parse_pbp <- function(boxscore_json) {
+parse_boxscore <- function(boxscore_json) {
 
   browser()
-  # # extract the plays from the boxscore_json
-  # plays <- boxscore_json$plays
-  #
-  # # create empty lists for the data from each play, one of indeterminate length
-  # event_list <- as.list(replicate(length(plays), data.frame()))
-  # details_list <- list()
-  #
-  # # loop through the plays
-  # for (i in seq_along(plays)) {
-  #
-  #   # extract play from list
-  #   p <- plays[[i]]
-  #
-  #   # every play should have these fields
-  #   event_df <- data.frame(
-  #
-  #     eventId = p[['eventId']],
-  #     period = p[['period']],
-  #     periodDescriptor = p[['periodDescriptor']],
-  #     timeInPeriod = p[['timeInPeriod']],
-  #     timeRemaining = p[['timeRemaining']],
-  #     situationCode = p[['situationCode']],
-  #     homeTeamDefendingSide = p[['homeTeamDefendingSide']],
-  #     typeCode = p[['typeCode']]
-  #
-  #   )
-  #
-  #   # stash the list of stuff everybody should have
-  #   event_list[[i]] <- event_df
-  #
-  #   # most plays will also have a field for details
-  #   if ('details' %in% names(p)) {
-  #
-  #     # extract the details list
-  #     details <- p[['details']]
-  #     # append the eventId so it can be merged later
-  #     details[['eventId']] <- p[['eventId']]
-  #     # append to list of details
-  #     details_list[[length(details_list)+1]] <- details
-  #
-  #   }
-  #
-  # }
-  #
-  # # bind all the events together into one big df
-  # all_events_df <- dplyr::bind_rows(event_list)
-  #
-  # # bind all the details into a big df
-  # all_details_df <- dplyr::bind_rows(details_list)
-  #
-  # # merge into one big boxscore df and return
-  # boxscore_df <- dplyr::full_join(all_events_df, all_details_df, by = 'eventId')
-  #
-  # return(boxscore_df)
+  # extract the boxscore
+  boxscore_list <- boxscore_json$boxscore
+
+  # loop through teams and positions to get player stats by game
+  player_stats <- boxscore_list$playerByGameStats
+  team_output_list <- replicate(length(player_stats), list())
+  names(team_output_list) <- names(player_stats)
+  for (team in names(player_stats)) {
+
+    team_stats <- player_stats[[team]]
+
+
+  }
+
+
+
+  return(boxscore_df)
 
 }
