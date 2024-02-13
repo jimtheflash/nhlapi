cbd44c52deb05b3d08cae30fd36838df2015206f
Jim Kloet <jim.kloet@gmail.com>
2024-02-13 10:24:31 -0600

    update summaries again

--- R/summaries.R
+++ R/summaries.R
@@ -21,9 +21,18 @@ boxscore_team_totals <- function(parsed_boxscore) {
         dplyr::any_of(cols_to_sum),
         list(team_game = sum_rm_na)
       )
-    )
-
-  output[is.na(output)] <- 0
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(
+      game_id = as.character(game_id),
+      team_id = as.character(team_id)
+    ) |>
+    dplyr::mutate(
+      dplyr::across(
+        tidyselect::where(is.numeric),
+        ~tidyr::replace_na(.x, 0)
+        )
+      )
 
   return(output)
 
@@ -40,11 +49,11 @@ first_goal <- function(parsed_pbp) {
     ) |>
     dplyr::filter(dplyr::row_number()==1) |>
     dplyr::transmute(
-      game_id,
-      first_goal_period = period,
-      first_goal_time_in_period_seconds = time_in_period_seconds,
-      first_goal_time_in_game_seconds = time_in_game_seconds,
-      first_goal_scorer = scoring_player_id
+      game_id = as.character(game_id),
+      first_goal_period = as.numeric(period),
+      first_goal_time_in_period_seconds = as.numeric(time_in_period_seconds),
+      first_goal_time_in_game_seconds = as.numeric(time_in_game_seconds),
+      first_goal_scorer = as.character(scoring_player_id)
     )
 
 }
@@ -62,7 +71,8 @@ player_offense_by_period <- function(parsed_pbp) {
       assists_p1 = dplyr::n_distinct(event_id[period==1]),
       assists_p2 = dplyr::n_distinct(event_id[period==2]),
       assists_p3 = dplyr::n_distinct(event_id[period==3]),
-      assists_ot = dplyr::n_distinct(event_id[!period %in% c(1,2,3)]))
+      assists_ot = dplyr::n_distinct(event_id[!period %in% c(1,2,3)])) |>
+    dplyr::ungroup()
 
   # shots ----
   shots <- parsed_pbp |>
@@ -88,7 +98,8 @@ player_offense_by_period <- function(parsed_pbp) {
       shots_ot = dplyr::n_distinct(event_id[!period %in% c(1,2,3)]),
       shots_on_goal_ot = dplyr::n_distinct(event_id[!period %in% c(1,2,3) & event_type=='shot_on_goal']),
       shots_missed_ot = dplyr::n_distinct(event_id[!period %in% c(1,2,3) & event_type=='missed_shot'])
-      )
+      ) |>
+    dplyr::ungroup()
 
   # goals ----
   goals <- parsed_pbp |>
@@ -105,12 +116,27 @@ player_offense_by_period <- function(parsed_pbp) {
       goals_p1 = dplyr::n_distinct(event_id[period==1]),
       goals_p2 = dplyr::n_distinct(event_id[period==2]),
       goals_p3 = dplyr::n_distinct(event_id[period==3]),
-      goals_ot = dplyr::n_distinct(event_id[!period %in% c(1,2,3)]))
+      goals_ot = dplyr::n_distinct(event_id[!period %in% c(1,2,3)])) |>
+    dplyr::ungroup()
 
   # join em up
   joined <- assists |>
     dplyr::full_join(shots, by = dplyr::join_by(player_id, game_id)) |>
-    dplyr::full_join(goals, by = dplyr::join_by(player_id, game_id))
+    dplyr::full_join(goals, by = dplyr::join_by(player_id, game_id)) |>
+    dplyr::mutate(
+      dplyr::across(
+        tidyselect::ends_with('_id'),
+        as.character
+      )
+    ) |>
+    dplyr::mutate(
+      dplyr::across(
+        tidyselect::where(is.numeric),
+        ~tidyr::replace_na(.x, 0)
+      )
+    )
+
+
 
   joined[is.na(joined)] <- 0
 
@@ -143,7 +169,8 @@ player_shifts_stats_by_period <- function(shifts) {
       toi_seconds_p1 = sum(shift_duration_seconds[period==1], na.rm = TRUE),
       toi_seconds_p2 = sum(shift_duration_seconds[period==2], na.rm = TRUE),
       toi_seconds_p3 = sum(shift_duration_seconds[period==3], na.rm = TRUE),
-      toi_seconds_ot = sum(shift_duration_seconds[!period %in% c(1,2,3)], na.rm = TRUE))
+      toi_seconds_ot = sum(shift_duration_seconds[!period %in% c(1,2,3)], na.rm = TRUE)) |>
+    dplyr::ungroup()
 
   # annoyingly, it is easier to get ppgs from here than other places so far
   pp_goals <- shifts |>
@@ -161,17 +188,25 @@ player_shifts_stats_by_period <- function(shifts) {
       goals_from_power_play_p2 = dplyr::n_distinct(id[period==2]),
       goals_from_power_play_p3 = dplyr::n_distinct(id[period==3]),
       goals_from_power_play_ot = dplyr::n_distinct(id[!period %in% c(1,2,3)])
-    )
+    ) |>
+    dplyr::ungroup()
 
   # combine for output
   output <- shift_summary |>
-    dplyr::left_join(pp_goals, by='player_id')
-
-  # id starters
-  output$starter_ind <- ifelse(output$player_id %in% starters, 1, 0)
-
-  # replace NAs with 0s
-  output[is.na(output)] <- 0
+    dplyr::left_join(pp_goals, by='player_id') |>
+    dplyr::mutate(starter_ind = dplyr::if_else(player_id %in% starters, 1, 0)) |>
+    dplyr::mutate(
+      dplyr::across(
+        tidyselect::ends_with('_id'),
+        as.character
+      )
+    ) |>
+    dplyr::mutate(
+      dplyr::across(
+        tidyselect::where(is.numeric),
+        ~tidyr::replace_na(.x, 0)
+      )
+    )
 
   return(output)
 
